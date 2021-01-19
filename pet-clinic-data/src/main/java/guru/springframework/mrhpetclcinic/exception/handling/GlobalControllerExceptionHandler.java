e1b95e71d87adf70fcbce8ac70153883b1697c67
Mohammad Hasan <rakibcse05@gmail.com>
2021-01-18 22:02:44 -0500

    update exception handling

--- pet-clinic-data/src/main/java/guru/springframework/mrhpetclcinic/exception/handling/GlobalControllerExceptionHandler.java
+++ pet-clinic-data/src/main/java/guru/springframework/mrhpetclcinic/exception/handling/GlobalControllerExceptionHandler.java
@@ -1,35 +1,105 @@
 package guru.springframework.mrhpetclcinic.exception.handling;
 
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 
 @RestControllerAdvice
-public class GlobalControllerExceptionHandler {
+public class GlobalControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
+  @Override
+  protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex,
+                                                                       HttpHeaders headers,
+                                                                       HttpStatus status,
+                                                                       WebRequest request) {
+    System.out.println(request.getHeaderNames());
+    ErrorDetails message = new ErrorDetails(
+            HttpStatus.METHOD_NOT_ALLOWED.value(),
+            new Date(),
+            ex.getLocalizedMessage() +"Supported methods are:"+ex.getSupportedHttpMethods(),
+            request.getDescription(false));
+    return new ResponseEntity(message,
+            new HttpHeaders(),
+            HttpStatus.METHOD_NOT_ALLOWED);
+  }
+  @ExceptionHandler(ResponseStatusException.class)
+  public ErrorDetails globalExceptionHandler(Exception ex, WebRequest request, HttpServletResponse response, HttpServletRequest req) {
+
+    ResponseStatusException responseStatusException = (ResponseStatusException) ex;
+    System.out.println(responseStatusException.getResponseHeaders());
+    response.setStatus(responseStatusException.getStatus().value());
+    System.out.println(req.getRequestURL() +" ==" +req.getRequestURI());
+    System.out.println(((ServletWebRequest)request).getRequest().getRequestURI());
+    System.out.println(((ServletWebRequest)request).getRequest().getRequestURL().toString());
+    ErrorDetails message = new ErrorDetails(
+            responseStatusException.getStatus().value(),
+            new Date(),
+            responseStatusException.getReason(),
+            request.getDescription(false));
+    
+    return message;
+  }
+
 
   @ExceptionHandler(ExceptionReason.class)
   @ResponseStatus(value = HttpStatus.NOT_FOUND)
   public ErrorDetails resourceNotFoundException(ExceptionReason ex, WebRequest request) {
     ErrorDetails message = new ErrorDetails(HttpStatus.NOT_FOUND.value(), new Date(),  ex.getMessage(),
             request.getDescription(false));
-    
+
     return message;
   }
 
+  /*@Override
+  protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex,
+                                                                HttpHeaders headers,
+                                                                HttpStatus status,
+                                                                WebRequest request) {
+    List<String> details = new ArrayList<>();
+    details.add(ex.getMethod());
+    details.add(" method is not supported for this request. Supported methods are "+ex.getSupportedHttpMethods());
+    //ex.getSupportedHttpMethods().forEach(t -> details.add(t + " "));
 
-  @ExceptionHandler(Exception.class)
-  @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
-  public ErrorDetails globalExceptionHandler(Exception ex, WebRequest request) {
-    ErrorDetails message = new ErrorDetails(
-        HttpStatus.INTERNAL_SERVER_ERROR.value(), new Date(), ex.getMessage(),
-            request.getDescription(false));
-    
-    return message;
-  }
+    ErrorResponse error = new ErrorResponse("Validation Failed", details);
+    return new ResponseEntity(error, HttpStatus.BAD_REQUEST);
+  }*/
+
+  /*@Override
+  protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex,
+                                                                       HttpHeaders headers,
+                                                                       HttpStatus status,
+                                                                       WebRequest request) {
+    StringBuilder builder = new StringBuilder();
+    builder.append(ex.getMethod());
+    builder.append(
+            " method is not supported for this request. Supported methods are ");
+    ex.getSupportedHttpMethods().forEach(t -> builder.append(t + " "));
+
+    ApiError apiError = new ApiError(HttpStatus.METHOD_NOT_ALLOWED,ex.getLocalizedMessage(), builder.toString());
+    return new ResponseEntity(apiError, new HttpHeaders(), HttpStatus.METHOD_NOT_ALLOWED);
+  }*/
+
+
+
+
 }
--- pet-clinic-web/src/main/java/guru/springframework/mrhpetclcinic/controller/OwnerRestController.java
+++ pet-clinic-web/src/main/java/guru/springframework/mrhpetclcinic/controller/OwnerRestController.java
@@ -4,11 +4,20 @@ import guru.springframework.mrhpetclcinic.exception.handling.ExceptionReason;
 import guru.springframework.mrhpetclcinic.model.Owner;
 import guru.springframework.mrhpetclcinic.service.OwnerServiceImpl;
 import guru.springframework.mrhpetclcinic.service.serviceinterface.OwnerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.Collection;
 import java.util.Set;
 
@@ -32,59 +41,102 @@ public class OwnerRestController {
     // http://localhost:7070/rest/owners
     @GetMapping("/owners")
     @ResponseBody
-    public ResponseEntity<Set<Owner>>listAllOwners(){
+    public ResponseEntity<Set<?>>listAllOwners(){
         Set<Owner> owners = ownerService.findAll();
         return new ResponseEntity<>(owners, HttpStatus.OK);
     }
     // findbyLastName using queryParam
     // http://localhost:7070/rest/findByLastNameUsingQueryParam/?lastName=Gleanne
+
+    @Operation(summary = "Find the Owner using Last Name by QueryParam")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Found the Owner",
+                    content = {
+                            @Content(mediaType = "application/json",schema = @Schema(implementation = Owner.class))
+                    }),
+            @ApiResponse(responseCode = "400",
+                        description = "Invalid LastName supplied",
+                        content = @Content),
+            @ApiResponse(responseCode = "404",
+                        description = "Owner not found",
+                        content = @Content) })
     @GetMapping("/findByLastNameUsingQueryParam")
-    @ResponseBody
-    public ResponseEntity<Owner> findOwnerByLastNameUsingQueryParam(@RequestParam(name = "lastName") String lastName){
+
+    public  @ResponseBody ResponseEntity<?> findOwnerByLastNameUsingQueryParam(
+                                            @RequestParam(name = "lastName")  String lastName){
         Owner owner = ownerService.findByLastName(lastName);
         return new ResponseEntity<>(owner, HttpStatus.OK);
     }
 
     // findbyLastName using pathVariable
     // http://localhost:7070/rest/findByLastNameUsingPathVariable/Gleanne
+    @Operation(summary = "Find the Owner using Last Name by pathVariable")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Found the Owner",
+                    content = {
+                            @Content(mediaType = "application/json",schema = @Schema(implementation = Owner.class))
+                    }),
+            @ApiResponse(responseCode = "400",
+                         description = "Invalid LastName supplied",
+                         content = @Content),
+            @ApiResponse(responseCode = "404",
+                    description = "Owner not found",
+                    content = @Content) })
     @GetMapping("/findByLastNameUsingPathVariable/{lastName}")
-    @ResponseBody
-    public ResponseEntity<Owner> findOwnerByLastNameUsingPathVariable(@PathVariable(name = "lastName") String lastName){
+    public @ResponseBody ResponseEntity<?> findOwnerByLastNameUsingPathVariable(
+                                            @PathVariable(name = "lastName") @NotBlank String lastName){
         Owner owner = ownerService.findByLastName(lastName);
         if(owner == null){
-            throw new ExceptionReason("There is no owner with lastName:"+ lastName);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"There is no owner with lastName:"+ lastName);
         }
         return new ResponseEntity<>(owner, HttpStatus.OK);
     }
 
     // findbyLastName using pathVariable
     // http://localhost:7070/rest/findByLastNameUsingCustomQuery/Gleanne
+    @Operation(summary = "Find the Owner using Last Name by pathVariable and CustomQuery used")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Found the Owner",
+                    content = {
+                            @Content(mediaType = "application/json",schema = @Schema(implementation = Owner.class))
+                    }),
+            @ApiResponse(responseCode = "400",
+                    description = "Invalid LastName supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404",
+                    description = "Owner not found",
+                    content = @Content) })
     @GetMapping("/findByLastNameUsingCustomQuery/{lastName}")
-    @ResponseBody
-    public ResponseEntity<Collection<Owner>> findOwnerByLastNameUsingCustomQuery(
-            @PathVariable(name = "lastName") String lastName){
+    public @ResponseBody ResponseEntity<Collection<?>> findOwnerByLastNameUsingCustomQuery(
+                                                        @PathVariable(name = "lastName") String lastName){
         Collection<Owner> owner = ownerService.findByLastNameUsingCustomQuery(lastName);
+        if (owner.size()==0){
+            throw new ExceptionReason("There is no owner with lastName:"+ lastName );
+        }
         return new ResponseEntity<>(owner, HttpStatus.OK);
     }
 
     // findbyid using queryParam
     // http://localhost:7070/rest/findByIdUsingQueryParam/?id=1
     @GetMapping("/findByIdUsingQueryParam/")
-    public ResponseEntity<Owner> findOwnerByIdUsingQueryParam(@RequestParam(name = "id") Long id){
+    public ResponseEntity<?> findOwnerByIdUsingQueryParam(@RequestParam(name = "id") Long id){
         return new ResponseEntity<>(ownerService.findById(id),HttpStatus.OK);
     }
 
     // findbyid using pathvariable
     // http://localhost:7070/rest/findByIdUsingPathVariable/1
     @GetMapping("/findByIdUsingPathVariable/{id}")
-    public ResponseEntity<Owner> findOwnerByIdUsingPathVariable(@PathVariable(name = "id") Long id){
+    public ResponseEntity<?> findOwnerByIdUsingPathVariable(@PathVariable(name = "id") Long id){
         return new ResponseEntity<>(ownerService.findById(id),HttpStatus.OK);
     }
 
     // findbyid using pathvariable
     // http://localhost:7070/rest/findByIdUsingCustomQuery/1
     @GetMapping("/findByIdUsingCustomQuery/{id}")
-    public ResponseEntity<Owner> findOwnerByIdUsingfindByIdUsingCustomQuery(@PathVariable(name = "id") Long id){
+    public ResponseEntity<?> findOwnerByIdUsingfindByIdUsingCustomQuery(@PathVariable(name = "id") Long id){
         Owner owner = ownerService.findByIdUsingCustomQuery(id);
         return new ResponseEntity<>(owner,HttpStatus.OK);
     }
@@ -92,7 +144,7 @@ public class OwnerRestController {
     // add owner
     //http://localhost:7070/rest//add/owner/
     @PostMapping("/add/owner/")
-    public ResponseEntity<Owner> addOwners(@RequestBody Owner owner){
+    public ResponseEntity<?> addOwners(@RequestBody Owner owner){
         Owner ownerToSaved = ownerService.save(owner);
         return new ResponseEntity<>(ownerService.findById(ownerToSaved.getId()),HttpStatus.CREATED);
     }
@@ -100,7 +152,7 @@ public class OwnerRestController {
     // edit or update owner
     //http://localhost:7070/rest/edit/owner/1
     @PutMapping("/edit/owner/{ownerid}")
-    public ResponseEntity<Owner> updateOwner(@RequestBody Owner owner, @PathVariable(name = "ownerid") Long ownerid){
+    public ResponseEntity<?> updateOwner(@RequestBody Owner owner, @PathVariable(name = "ownerid") Long ownerid){
         Owner requestOwner = ownerService.findById(ownerid);
         requestOwner.setFirstName(owner.getFirstName());
         requestOwner.setLastName(owner.getLastName());
